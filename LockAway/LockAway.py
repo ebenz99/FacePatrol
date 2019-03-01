89ee54d7ff770034b34b709e4a97608c85b135af
Ethan Bensman <ebenz99@gmail.com>
2019-02-28 21:44:25 -0500

    increasing wait time

--- LockAway/LockAway.py
+++ LockAway/LockAway.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2
 import subprocess
 import sys
+import time
 
 # Set up variables
 cap = cv2.VideoCapture(0)
@@ -13,38 +14,42 @@ lockCounter = 0
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 
 while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	
+	# Convert fram to black and white image
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	cv2.imshow('frame',gray)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+	# Draw rectangle on face, not necessary
+	for (x,y,w,h) in faces:   
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)  
+		roi_gray = gray[y:y+h, x:x+w] 
+		roi_color = frame[y:y+h, x:x+w] 
 
-    # Convert fram to black and white image
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Determine if face in in view, if not lock Mac
+	if len(faces) > 0:
+		lockCounter = 0
+	else:
+		lockCounter = lockCounter + 1
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	# This value determines how many frames pass without a face before Mac locks
+	if lockCounter >= 10:
+	# I tried to avoid using shell=True here but I could not find a way around it.
+		subprocess.call('/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend',shell=True)
+		sys.exit()
+	
+	# Display the resulting frame, used for testing
+	cv2.imshow('LockAway', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	time.sleep(2)
 
-    # Draw rectangle on face, not necessary
-    for (x,y,w,h) in faces:   
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)  
-        roi_gray = gray[y:y+h, x:x+w] 
-        roi_color = frame[y:y+h, x:x+w] 
 
-    # Determine if face in in view, if not lock Mac
-    if len(faces) > 0:
-        lockCounter = 0
-    else:
-        lockCounter = lockCounter + 1
-
-    # This value determines how many frames pass without a face before Mac locks
-    if lockCounter >= 10:
-        # I tried to avoid using shell=True here but I could not find a way around it.
-        subprocess.call('/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend',
-            shell=True)
-        sys.exit()
-
-    # Display the resulting frame, used for testing
-    cv2.imshow('LockAway', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
 # Clean up
 cap.release()
